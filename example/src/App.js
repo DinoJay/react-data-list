4616ceffd2b483426166fd71ec79c0d00249d7e7
Jan Maushagen <jan@wise77.vub.ac.be>
2020-01-13 11:27:39 +0100

    basic portal of dropdown

--- example/src/App.js
+++ example/src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react'
 
 import ExampleComponent from 'react-data-list'
 
+const values = Array.from(new Array(30), (_, i) => i)
+
 export default class App extends Component {
   render () {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div style={{overflow:'hidden'}}>
+        <div style={{width: '18rem', height:'18rem', border: '2px solid black', overflow:'hidden'}}>
+          <ExampleComponent listStyle={{border: '2px solid black', width: '100%'}}>
+            {values.map(d => <div>{d}</div>)}
+          </ExampleComponent>
+        </div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,57 @@
-import React, { Component } from 'react'
+import React, { useState, useRef, useEffect} from 'react'
 import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
 
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
+export default function DataList(props) {
+  const {children, listClassName, listStyle, className, style} = props
+  const [open, setOpen] = useState(false)
+  const [bbox, setBbox] = useState({x: 0, y: 0})
+  const refInput = React.useRef(null)
 
-  render() {
-    const {
-      text
-    } = this.props
+  const getBBox = () =>
+    refInput.current
+      ? refInput.current.getBoundingClientRect()
+      : {x: 0, y: 0, width: 0, height: 0}
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+  const list = <ul
+    style={{
+
+      width: getBBox().width - 4,
+      listStyle: 'none',
+      marginBlockStart: 0,
+      marginBlockEnd: 0,
+      // margin-inline-start: 0px;
+      // margin-inline-end: 0px;
+      paddingInlineStart: 0,
+
+      position: 'absolute',
+      left: getBBox().x,
+      top: getBBox().y + getBBox().height,
+      display: !open && 'none',
+      margin: 0,
+      marginTop: 4
+    }}>
+    <div className={listClassName} style={listStyle}>
+      {
+        React.Children.map(children, d => <li>{d}</li>)
+      }
+    </div>
+  </ul>
+
+  const renderedList = ReactDOM
+    .createPortal(list, document.querySelector('body'))
+
+  return (
+    <div style={{padding: 10}}>
+      <input
+        ref={refInput}
+        style={{border: '2px solid black', padding: '10px'}}
+        onFocus={() => setOpen(!open)}
+        onBlur={() => setOpen(false)}
+      />
+      {renderedList}
+    </div>
+  )
 }
